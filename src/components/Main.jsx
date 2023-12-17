2da904005400758fea71457b48c68f2192f06c70
Faruqoloyede <oloyedefaruq2@gmail.com>
2023-12-17 12:30:12 -0800

    added hero section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,8 +4,11 @@ const Main = () => {
   return (
     <section id="home">
       <div className={`flex md:flex-row flex-col items-center ${styles.paddingY}`}>
-        <h4>GULLU PIZZA</h4>
-        <img src={her} className="w-[300px] h-[300px] relative z-[5]"/>
+        <div className={`${styles.paddingX}`}>
+          <h4 className="font-poppins text-right text-white font-bold text-[20px]">GULLU PIZZA</h4>
+          <h1 className={`${styles.heading1}`}>FREE HOME <br/><span className="text-red">DELIVERY</span></h1>
+        </div>
+        <img src={her} className="w-[50%] h-[50%] relative z-[5]"/>
       </div>
     </section>
   )
--- src/style.js
+++ src/style.js
@@ -1,7 +1,7 @@
 const styles = {
     boxWidth: "xl:max-w-[1280px] w-full",
 
-    heading1: "font-poppins font-semibold xs:text-[50px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+    heading1: "font-poppins font-semibold xs:text-[60px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
 
     paragraph: "font-poppins font-normal text-white text-[18px] leading-[30.8px]",
 
@@ -13,5 +13,7 @@ const styles = {
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:mx-16 mx-6",
 }
+
+
 
 export default styles
